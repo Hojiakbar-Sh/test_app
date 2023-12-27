1f3bfb078648beb7bf1e02c838e2a3c42354210a
Hojiakbar-Sh <hojiakbarshirinov70@gmail.com>
2023-12-27 20:14:08 +0500

    Fixed: 27:12:23 20:14

--- build_files.sh
+++ build_files.sh
@@ -9,11 +9,6 @@ python3.9 pip install -U pip
 echo "Installing project dependencies..."
 python3.9 -m pip install -r requirements.txt
 
-# Make migrations
-echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
 # Collect staticfiles
 echo "Collect static..."
 python3.9 manage.py collectstatic --noinput --clear
